5de06b1d8bb7e9faa3336f31764eb45bdbeb455e
Kevin Leon <Kepin33@gmail.com>
2020-05-12 11:08:03 +0700

    new package

--- resources/views/user/package.blade.php
+++ resources/views/user/package.blade.php
@@ -2,48 +2,63 @@
 
 @section('content')
 
-    <div class="container py-5">
-        <div class="row justify-content-center">
-            <div class="">
-                <div class="card">
-                    <div class="card-header"><h3>Package</h3></div>
-                    <div class="card-body mx-auto">
+<div class="container py-5">
 
 
-                    <table class="tablePackage table" style="margin: 1.5em;"  >
-{{--                        @foreach($packages as $p)--}}
-{{--                            <tr>--}}
-{{--                                <td class="profile-member-list"> <li style="list-style: none;">{{$p->package_cost}}</li></td>--}}
-{{--                                <td class="profile-member-list"> <li style="list-style: none;">{{$p->max_balance}}</li></td>--}}
-{{--                                <td class="profile-member-list"> <li style="list-style: none;">{{$p->max_withdraw}}</li></td>--}}
-{{--                            </tr>--}}
-{{--                        @endforeach--}}
-                    </table>
-                </div>
-                    <div class="card-footer">
-                        <div>Add Package</div>
-                        <table class="table">
-                            <tr>
-                                <th > <li style="list-style: none;">Package Cost</li></th>
-                                <th > <li style="list-style: none;">Max Balance</li></th>
-                                <th > <li style="list-style: none;">Max Withdraw</li></th>
-                            </tr>
-                            <tr>
-                                <td><input type="number" name="addPackageCost" id="addPackageCost"></td>
-                                <td><input type="number" name="addMaxBalance" id="addMaxBalance"></td>
-                                <td><input type="number" name="addMaxWithdraw" id="addMaxWithdraw"></td>
-                            </tr>
-                        </table>
-                        <div>
-                            <button class="btn btn-secondary" id="btnAdd" >Add</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+
+    <div class="row col-10">
+        <h3>Package</h3>
     </div>
-    <script src="{{asset('js/jquery-3.3.1.min.js')}}"></script>
-    <script>
+    <div class="row mx-auto ">
+
+
+        <table class="tablePackage table" style="margin: 1.5em;">
+            {{--                        @foreach($packages as $p)--}}
+            {{--                            <tr>--}}
+            {{--                                <td class="profile-member-list"> <li style="list-style: none;">{{$p->package_cost}}
+            </li>
+            </td>--}}
+            {{--                                <td class="profile-member-list"> <li style="list-style: none;">{{$p->max_balance}}
+            </li>
+            </td>--}}
+            {{--                                <td class="profile-member-list"> <li style="list-style: none;">{{$p->max_withdraw}}
+            </li>
+            </td>--}}
+            {{--                            </tr>--}}
+            {{--                        @endforeach--}}
+        </table>
+    </div>
+    <div class="row col-10 mx-auto">
+        <div class="col-12"><h3> Add Package</h3></div>
+       
+            <form class="row">
+
+                <div class="form-group col-lg-4">
+                    <label style="list-style: none;">Package Cost</label>
+                    <input type="number" class="form-control" name="addPackageCost" id="addPackageCost">
+                </div>
+                <div class="form-group col-lg-4">
+                    <label style="list-style: none;">Max Balance</label>
+                    <input type="number" class="form-control" name="addMaxBalance" id="addMaxBalance">
+                </div>
+                <div class="form-group col-lg-4">
+                    <label style="list-style: none;">Max Withdraw</label>
+                    <input type="number" class="form-control" name="addMaxWithdraw" id="addMaxWithdraw">
+                </div>
+                <button class="btn btn-secondary" id="btnAdd">Add</button>
+            </form>
+
+       
+
+
+
+
+
+    </div>
+
+</div>
+<script src="{{asset('js/jquery-3.3.1.min.js')}}"></script>
+<script>
     $(document).ready(function () {
         // var package = [];
         var packageList = {!! json_encode($packages->toArray()) !!};
@@ -63,7 +78,7 @@
                     '<td > <li style="list-style: none;">'+packageList[i].package_cost+'</li></td>'+
                     '<td > <li style="list-style: none;">'+packageList[i].max_balance+'</li></td>'+
                     '<td > <li style="list-style: none;">'+packageList[i].max_withdraw+'</li></td>'+
-                    '<td > <li style="list-style: none;"><button onclick="" class="btnDelete btn btn-secondary" id="btnDeletePackage'+i+'">Remove</button></li></td>' +
+                    '<td class="mx-auto" > <li style="list-style: none;"><button onclick="" class="btnDelete btn btn-secondary" id="btnDeletePackage'+i+'">Remove</button></li></td>' +
                     '</tr>'
             }
             $('.tablePackage').html(packageDiv);
@@ -108,6 +123,6 @@
             });
         });
     });
-    </script>
+</script>
 
 @endsection
